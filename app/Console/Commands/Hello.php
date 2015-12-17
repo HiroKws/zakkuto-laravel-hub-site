c083341bf8cefed8a582b4a83c73668a3072cd0a
Hirohisa Kawase <hirokws@gmail.com>
2015-12-17 13:58:04 +0900

    引数の利用

--- app/Console/Commands/Hello.php
+++ app/Console/Commands/Hello.php
@@ -6,7 +6,9 @@ use Illuminate\Console\Command;
 
 class Hello extends Command
 {
-    protected $signature = 'hello:world';
+    protected $signature = 'hello:world '
+        .'{name : 挨拶する人や物。} '
+        .'{greeting=さん、ようこそ : 挨拶の言葉。}';
 
     protected $description = 'はじめの一歩、Hello Worldを出力する。';
 
@@ -17,6 +19,6 @@ class Hello extends Command
 
     public function handle()
     {
-        echo 'Hello World!!'.PHP_EOL;
+        echo $this->argument('name').$this->argument('greeting').PHP_EOL;
     }
 }
