ecc95a3457c40dd9c48991b1fd3f556d6ac43edb
Hirohisa Kawase <hirokws@gmail.com>
2015-12-17 16:57:41 +0900

    ワイルドカードで複数イベントをマップする確認

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,7 +17,7 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Reminder::class => [EchoEvent::class],
+        Reminder::class.'*' => [EchoEvent::class],
 #        Reminder::class  => [EchoEvent::class, EchoEvent2::class],
 #        Reminder2::class => [EchoEvent2::class],
     ];
