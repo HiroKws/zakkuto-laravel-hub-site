8adc7890a87cb3ca1d6851aab3c52900875fed4e
Hirohisa Kawase <hirokws@gmail.com>
2015-12-17 13:15:56 +0900

    必要なファイルをコピー

---
+++ helper.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!function_exists('__')) {
+    function __($message)
+    {
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            return mb_convert_encoding($message, 'SJIS', 'UTF-8');
+        }
+
+        return $message;
+    }
+}
